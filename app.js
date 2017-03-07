464a07ac378cadce07be4ca93c7688e9ef34be40
esantonja <enrique.santonja@travelport.com>
2017-03-07 14:32:41 +0100

    Added easy http get server. Updated gitignore and package.json files added Express as dependency

---
+++ app.js
@@ -0,0 +1,45 @@
+var port = 1979;
+var http = require('http');
+
+function addTrace(start, status) {
+    console.log("Process end");
+    var end = new Date();
+    var lapse = end - start;
+    // TODO: Store in mongo
+    console.log("Status Response: "+ status +" Time Lapse = " +lapse + "ms");
+}
+
+
+var server = http.createServer(function (request, response) {
+    var url = require('url');
+    var destinationHost = url.parse(request.url, true).query.url;
+
+    if(destinationHost === undefined) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end("Invalid URL: " + destinationHost);
+    }
+    else {
+      // TODO: Add url validation.
+      console.log("Process start");
+
+      var dateTimeStart = new Date();
+      var session = new AAGUID
+      var options = {
+                    host: destinationHost,
+                    port: 80,
+                    path: '/'
+                };
+
+        http.get(options, function(res) {
+            res.on("data", function(chunk) {
+                addTrace(dateTimeStart, res.statusCode)              
+            });
+        }).on('error', function(e) {
+            addTrace(dateTimeStart, e.statusCode)
+        });
+    }
+});
+
+server.listen(port);
+
+console.log("Server running at http://localhost:" + port + "/");
