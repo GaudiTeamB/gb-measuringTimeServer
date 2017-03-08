fc470a3a746fc12b24f7aa313b7e644bafa383e4
esantonja <enrique.santonja@travelport.com>
2017-03-08 11:36:33 +0100

    Added test infrastructure dependecies to package.json and dummy test example in test.js

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
 var port = 1979;
+var logic = require('./logic');
 var http = require('http');
 
-function addTrace(start, status) {
-    console.log("Process end");
-    var end = new Date();
-    var lapse = end - start;
-    // TODO: Store in mongo
-    console.log("Status Response: "+ status +" Time Lapse = " +lapse + "ms");
-}
-
-
 var server = http.createServer(function (request, response) {
-    var url = require('url');
-    var destinationHost = url.parse(request.url, true).query.url;
 
-    if(destinationHost === undefined) {
+    var destinationHost = logic.retrieveUrl(request);
+ 
+    if(destinationHost === undefined || !logic.isValidUrl(destinationHost)) {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.end("Invalid URL: " + destinationHost);
     }
     else {
-      // TODO: Add url validation.
-      console.log("Process start");
-
-      var dateTimeStart = new Date();
-      
-      var options = {
-                    host: destinationHost,
-                    port: 80,
-                    path: '/'
-                };
-
-        http.get(options, function(res) {
-            res.on("data", function(chunk) {
-                addTrace(dateTimeStart, res.statusCode)              
-            });
-        }).on('error', function(e) {
-            addTrace(dateTimeStart, e.statusCode)
-        });
+        var start = new Date();
+        // TODO: Store in mongo StartTime
+        console.log("Start Time: " + start);
+        logic.httpCall(destinationHost).then(status => logic.addTrace(start, status)).catch((error) => logic.addTrace(start, 500));     
     }
 });
 
