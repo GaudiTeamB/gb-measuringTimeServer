4d688c00460fe936431b77f5349b85b52626a424
Oriol <ograu@pasiona.com>
2017-03-10 10:40:11 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ app.get('/', function (request, response) {
    var destinationHost = logic.retrieveUrl(request);
  
     if(destinationHost === undefined || !logic.isValidUrl(destinationHost)) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.end("Invalid URL: " + destinationHost);
+        logic.sendResponse(response, 404, "Invalid URL: " + destinationHost)
     }
     else {
         var start = new Date();
         // TODO: Store in mongo StartTime
         console.log("Start Time: " + start);
-        logic.httpCall(destinationHost).then(status => logic.addTrace(start, status)).catch((error) => logic.addTrace(dateTimeStart, 500));     
+        logic.httpCall(destinationHost).then(function(status) { logic.sendResponse(response, status, "Ok"); logic.addTrace(start, status); }).catch(function(message) { logic.sendResponse(response, 500, message); logic.addTrace(start, message); });   
     }
 })
 
